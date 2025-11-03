0e58366380e8c092a8021d5974e40ee12a994c6e
Mahedi-432A <mdmahedih038@gmail.com>
2025-11-03 12:23:32 +0600

    feat: Enhance PrivateRoute to include loading state handling

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,8 +3,12 @@ import {  AuthContext  } from '../context/AuthContext/AuthContext';
 import { Navigate, useLocation } from 'react-router';
 
 const PrivateRoute = ({children}) => {
-    const {user} = use(AuthContext);
+    const {user, loading} = use(AuthContext);
     const location = useLocation();
+
+    if(loading){
+      return <span className="loading loading-ring loading-xl"></span>
+    }
 
     if (!user) {
       return  <Navigate to='/login' state={location.pathname}></Navigate>
