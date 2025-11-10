dc4c5390062ef5161db8c28fb01f7fc11880f880
Mahedi-432A <mdmahedih038@gmail.com>
2025-11-10 15:57:25 +0600

    feat: Enhance AddJob component with detailed job form including fields for title, company, location, logo, job type, category, application deadline, and salary range

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,12 +1,169 @@
-import React from 'react'
+import React from "react";
 
 const AddJob = () => {
   return (
     <div>
-        <h2>Please add a job</h2>
-        <form></form>
-    </div>
-  )
-}
+      <h2>Please add a job</h2>
+      <form>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
 
-export default AddJob
+          <label className="label">Job Title</label>
+          <input
+            type="text"
+            name="title"
+            className="input"
+            placeholder="Job Title"
+          />
+
+          <label className="label">Comoany Name</label>
+          <input
+            type="text"
+            name="company"
+            className="input"
+            placeholder="Companhy Name"
+          />
+
+          <label className="label">Location</label>
+          <input
+            type="text"
+            name="location"
+            className="input"
+            placeholder="Company Location"
+          />
+
+          <label className="label">Comoany Logo</label>
+          <input
+            type="text"
+            name="company_logo"
+            className="input"
+            placeholder="Companhy Logo URL"
+          />
+        </fieldset>
+
+        {/* Job Type */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Job Type</legend>
+
+          <div className="filter">
+            <input
+              className="btn filter-reset"
+              type="radio"
+              name="jobType"
+              aria-label="All"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="On-Site"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="Remote"
+            />
+            <input
+              className="btn"
+              type="radio"
+              name="jobType"
+              aria-label="Hybrid"
+            />
+          </div>
+        </fieldset>
+
+        {/* Job Category */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Job Category</legend>
+
+          <select
+            defaultValue="Job Category"
+            name="category"
+            className="select"
+          >
+            <option disabled={true}>Job Category</option>
+            <option>Engineering</option>
+            <option>Developer</option>
+            <option>Designer</option>
+            <option>HR</option>
+            <option>Finance</option>
+            <option>SEX</option>
+          </select>
+        </fieldset>
+
+        {/* Application Deadline */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Application Deadline</legend>
+
+          <input type="date" name="applicationDeadline" className="input" />
+        </fieldset>
+
+        {/* Salary Range */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Salary Range</legend>
+
+          <div className="grid gap-4 grid-cols-1 md:grid-cols-3 items-center">
+            <div>
+              <label className="label">Minimum Salary</label>
+              <input
+                type="text"
+                name="salaryMin"
+                className="input"
+                placeholder="Minimum Salary"
+              />
+            </div>
+
+            <div>
+              <label className="label">Maximum Salary</label>
+              <input
+                type="text"
+                name="salaryMax"
+                className="input"
+                placeholder="Maximum Salary"
+              />
+            </div>
+
+            <div>
+                <label className="label">Select Currency</label>
+              <select
+                defaultValue="Currency"
+                name="currency"
+                className="select"
+              >
+                <option disabled={true}>Currency</option>
+                <option>BDT</option>
+                <option>USD</option>
+                <option>EUR</option>
+                <option>Youan</option>
+                <option>Rubol</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        {/*  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+        </fieldset>
+
+        {/*  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+        </fieldset>
+
+        {/*  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+        </fieldset>
+
+        {/*  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Basic Info</legend>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default AddJob;
