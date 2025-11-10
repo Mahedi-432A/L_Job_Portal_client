ef033ab6d9e04eb53e49b07ec79f9f6d41b08460
Mahedi-432A <mdmahedih038@gmail.com>
2025-11-10 12:47:31 +0600

    feat: Implement MyApplications feature with ApplicationList and JobApplicationRow components; integrate API for fetching user applications

--- src/pages/MyApplications/ApplicationList.jsx
+++ src/pages/MyApplications/ApplicationList.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { use } from "react";
+import JobApplicationRow from "./JobApplicationRow";
 
-const ApplicationList = () => {
+const ApplicationList = ({ myApplicationPromise }) => {
+  const applications = use(myApplicationPromise);
   return (
-    <div>ApplicationList</div>
-  )
-}
+    <div>
+      <h2>JOBs applied so far {applications.length}</h2>
 
-export default ApplicationList
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  #
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* table body rows here-----------/. */}
+            {applications.map((application, index) => (
+              <JobApplicationRow
+                key={application._id}
+                index={index}
+                application={application}
+              ></JobApplicationRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ApplicationList;
